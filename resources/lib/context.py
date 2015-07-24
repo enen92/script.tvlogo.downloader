c8e6766750e88d6538c33f918df2156fb9da4d34
enen92 <92enen@gmail.com>
2015-07-24 03:48:08 +0100

    bug fix

--- resources/lib/context.py
+++ resources/lib/context.py
@@ -50,22 +50,22 @@ def run(channel_name):
 				match = return_only_valid(match)
 				if not match:
 					#check if nonascii version exists
-					match = thelogodb.Channels().by_keyword(urllib.quote_plus(removeNonAscii(channel_name)))
+					match = thelogodb.Channels().by_keyword(urllib.quote_plus(removeNonAscii(urllib.unquote_plus(channel))))
 					match = return_only_valid(match)
 					#if no match check if channel is HD and grab logos for nonhd
 					if not match:
-						match = tvlogodownloader.get_nonhd_match(removeNonAscii(channel_name))
+						match = tvlogodownloader.get_nonhd_match(removeNonAscii(urllib.unquote_plus(channel)))
 						match = return_only_valid(match)
 		else:
 			match = tvlogodownloader.get_nonhd_match(urllib.unquote_plus(urllib.unquote_plus(channel)))
 			match = return_only_valid(match)
 			if not match:
 				#check if nonascii version exists
-				match = thelogodb.Channels().by_keyword(urllib.quote_plus(removeNonAscii(channel_name)))
+				match = thelogodb.Channels().by_keyword(urllib.quote_plus(removeNonAscii(urllib.unquote_plus(channel))))
 				match = return_only_valid(match)
 				#if no match check if channel is HD and grab logos for nonhd
 				if not match:
-					match = tvlogodownloader.get_nonhd_match(removeNonAscii(channel_name))
+					match = tvlogodownloader.get_nonhd_match(removeNonAscii(urllib.unquote_plus(channel)))
 					match = return_only_valid(match)
 
 		if match:
